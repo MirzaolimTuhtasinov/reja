26a63484906fd5922c6d0e3262331ca1d1d1c6ce
MirzaolimTuhtasinov <mirzaolimtoxtasinov8@gmail.com>
2026-08-31 22:23:42 +0900

    fix: refractoring

--- server.js
+++ server.js
@@ -42,12 +42,12 @@ app.get('/author', (req, res) => {
 });
 
 app.get("/", function(req, res ) {
-    res.render("harid");
+    res.render("reja");
 });
 
 const server = http.createServer(app);
 let PORT = 3000;
 
 server.listen(PORT, () => {
-    console.log('The server is running succesfully on port:', PORT)
+    console.log(`The server is running succesfully on port: ${PORT}, http://localhost:${PORT}`)
 });
